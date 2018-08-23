415a1197de8feab46065fcf8091e84f2e3eb56e0
lord987 <204172@supinfo.com>
2018-08-23 08:40:23 +0200

    Add branch for nixos

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-icon= $HOME"/Pictures/avatar.png"
+#icon= $HOME"/Pictures/avatar.png"
 tmpbg='/tmp/screen.png'
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+convert "$tmpbg" "$HOME/Pictures/avatar.png" -geometry '-840+0' -gravity center -composite -matte "$tmpbg"
+#((EC1+EC2)/2)-(EC1/2)
+#xrandr | grep \* | awk '{print $1}' | awk -F "x" '{print $1}'
 
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 
 i3lock -u -i "$tmpbg" -d
 rm "$tmpbg"
